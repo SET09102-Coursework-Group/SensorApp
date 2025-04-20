b14975920589d4fb7a0934d6d463e7f91ae2a70d
Catriona Vine <catriona.vine@neonwavestudios.co.uk>
2025-04-20 15:53:15 +0100

    Add further menu factory tests for other roles

---
+++ SensorApp/SensorApp.Tests/UnitTests/Factory/EnvironmentalMenuFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using SensorApp.Shared.Factories;
+using SensorApp.Shared.Interfaces;
+
+namespace SensorApp.Tests.UnitTests.Factory;
+public class EnvironmentalMenuFactoryTests
+{
+    [Fact]
+    public void Environmental_CreateMenu_ShouldReturnExpectedMenuItems()
+    {
+        // Arrange
+        IMenuFactory factory = new EnvironmentalMenuFactory();
+
+        // Act
+        var menu = factory.CreateMenu().ToList();
+
+        // Assert
+        menu.Should().NotBeNullOrEmpty("Environmental scientist menu should have at least one item");
+
+        menu.Should().Contain(item => item.Route == "SensorMapPage");
+    }
+}
