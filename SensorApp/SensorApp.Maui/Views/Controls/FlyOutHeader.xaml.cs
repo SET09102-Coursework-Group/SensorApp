9610e89be72ad2c0dd79c0a19c4635b731aa1395
Nicole Soto <Nicole.Soto@stormid.com>
2025-04-21 17:14:21 +0100

    fix flyout menu for the user info - now it's centered and humized

--- SensorApp/SensorApp.Maui/Views/Controls/FlyOutHeader.xaml.cs
+++ SensorApp/SensorApp.Maui/Views/Controls/FlyOutHeader.xaml.cs
@@ -5,15 +5,10 @@ public partial class FlyOutHeader : StackLayout
     public FlyOutHeader()
     {
         InitializeComponent();
-        SetValues();
-    }
 
-    private void SetValues()
-    {
         if (App.UserInfo != null)
         {
-            lblUserName.Text = App.UserInfo.Username;
-            lblRole.Text = App.UserInfo.Role;
+            BindingContext = App.UserInfo;
         }
     }
 }
