0a2b8fe863ea5d1b4e227efabb96d9cc8453b73e
Nicole Soto <Nicole.Soto@stormid.com>
2025-04-23 17:23:56 +0100

    Working view model to get measurements and measurands values

--- SensorApp/SensorApp.Maui/ViewModels/HistoricalDataViewModel.cs
+++ SensorApp/SensorApp.Maui/ViewModels/HistoricalDataViewModel.cs
@@ -2,40 +2,72 @@
 using CommunityToolkit.Mvvm.Input;
 using SensorApp.Shared.Interfaces;
 using SensorApp.Shared.Models;
+using SensorApp.Shared.Services;
 using System.Collections.ObjectModel;
 
 namespace SensorApp.Maui.ViewModels;
 
 public partial class HistoricalDataViewModel : BaseViewModel
 {
-    private readonly IMeasurementService _service;
+    private readonly IMeasurementService _measurementService;
     private readonly ITokenProvider _tokenProvider;
+    private readonly IMeasurandService _measurandService;
+    private readonly SensorApiService _sensorService;
 
-    public HistoricalDataViewModel(IMeasurementService service, ITokenProvider tokenProvider)
+    public HistoricalDataViewModel(IMeasurementService measurementService, ITokenProvider tokenProvider, SensorApiService sensorService, IMeasurandService measurandService)
     {
-        _service = service;
+        _measurementService = measurementService;
         _tokenProvider = tokenProvider;
+        _sensorService = sensorService;
+        _measurandService = measurandService;
 
         From = DateTime.Today.AddDays(-7);
         To = DateTime.Today;
     }
 
-    [ObservableProperty]
+    [ObservableProperty] 
     DateTime? from;
 
-    [ObservableProperty]
+    [ObservableProperty] 
     DateTime? to;
 
     [ObservableProperty]
-    int? selectedSensorId;
+    SensorModel? selectedSensor;
 
     [ObservableProperty]
-    int? selectedMeasurandId;
+    MeasurandModel? selectedMeasurand;
 
-    [ObservableProperty]
+    [ObservableProperty] 
     bool isLoading;
 
-    public ObservableCollection<MeasurementModel> Points { get; } = [];
+    public ObservableCollection<MeasurementModel> MeasurementValues { get; } = [];
+    public ObservableCollection<SensorModel> SensorOptions { get; } = [];
+    public ObservableCollection<MeasurandModel> MeasurandOptions { get; } = [];
+
+
+    partial void OnSelectedSensorChanged(SensorModel? value)
+    {
+        _ = ReloadMeasurandsAsync(value);
+    }
+
+    private async Task ReloadMeasurandsAsync(SensorModel? sensor)
+    {
+        MeasurandOptions.Clear();
+        SelectedMeasurand = null;
+        if (sensor == null)
+        {
+            return;
+        }
+
+        var token = await _tokenProvider.GetTokenAsync();
+        var mesurandList = await _measurandService.GetMeasurandsAsync(token!, sensor.Id);
+
+        foreach (var mesurand in mesurandList)
+        {
+            MeasurandOptions.Add(mesurand);
+        }
+    }
+
 
     [RelayCommand]
     public async Task LoadAsync()
@@ -52,21 +84,37 @@ public partial class HistoricalDataViewModel : BaseViewModel
                 return;
             }
 
-            Points.Clear();
-            var list = await _service.GetMeasurementsAsync(sensorId: SelectedSensorId, measurandId: SelectedMeasurandId, from: From, to: To, token: token);
+            MeasurementValues.Clear();
+            var measurements = await _measurementService.GetMeasurementsAsync(sensorId: SelectedSensor?.Id, measurandId: SelectedMeasurand?.Id, from: From, to: To, token: token);
 
-            foreach (var measurement in list)
+            foreach (var measurement in measurements)
             {
-                Points.Add(measurement);
+                MeasurementValues.Add(measurement);
             }
         }
         catch (Exception ex)
         {
             await Shell.Current.DisplayAlert("Error", $"Could not load data: {ex.Message}", "OK");
         }
-        finally
+        finally 
+        { 
+            IsLoading = false; 
+        }
+    }
+
+    public async Task LoadLookupListsAsync()
+    {
+        var token = await _tokenProvider.GetTokenAsync();
+        if (string.IsNullOrWhiteSpace(token))
         {
-            IsLoading = false;
+            return;
+        }
+
+        SensorOptions.Clear();
+        var sensors = await _sensorService.GetSensorsAsync(token);
+        foreach (var sensor in sensors)
+        {
+            SensorOptions.Add(sensor);
         }
     }
 }
