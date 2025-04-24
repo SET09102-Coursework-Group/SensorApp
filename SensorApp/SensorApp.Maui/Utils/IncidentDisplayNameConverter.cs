70ccb8b448dd4a240f6fc902c81b41de10c80ec1
Catriona Vine <catriona.vine@neonwavestudios.co.uk>
2025-04-24 10:07:53 +0100

    Add converter for incident label

---
+++ SensorApp/SensorApp.Maui/Utils/IncidentDisplayNameConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using SensorApp.Shared.Dtos.Incident;
+
+namespace SensorApp.Maui.Utils;
+
+public class IncidentDisplayNameConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is IncidentDto incident)
+        {
+            return $"{incident.Sensor?.Type} Sensor {incident.Sensor?.Id} - {incident.Creation_date:dd MMM yyyy}";
+        }
+
+        return string.Empty;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+};
